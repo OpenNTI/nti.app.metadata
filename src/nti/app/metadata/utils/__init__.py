e1a13d2762fb5392a94df7306daf7c30a79c9b3b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-04 18:20:38 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.metadata/trunk@110131 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/metadata/utils/__init__.py
+++ src/nti/app/metadata/utils/__init__.py
@@ -106,8 +106,11 @@ def check_indices(catalog_interface=IMetadataCatalog, intids=None,
                                     len(processed), name, catalog)
                 elif IKeywordIndex.providedBy(index):
                     docids = list(index.ids())
-                    processed = _process_ids(
-                        catalogs, docids, missing, broken, seen)
+                    processed = _process_ids(catalogs,
+                                             docids, 
+                                             missing, 
+                                             broken, 
+                                             seen)
                     if processed:
                         logger.info("%s record(s) unindexed. Source %s,%s",
                                     len(processed), name, catalog)
