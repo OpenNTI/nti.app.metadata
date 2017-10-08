7548e3d187cd6d5ac8911eb827d772fd3695727c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-08 02:14:11 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.metadata/trunk@122887 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/metadata/generations/evolve6.py
+++ src/nti/app/metadata/generations/evolve6.py
@@ -4,12 +4,9 @@
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
-
-generation = 6
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope import component
 
@@ -21,6 +18,10 @@ from zope.intid.interfaces import IIntIds
 from nti.metadata import queue_event
 
 from nti.metadata.interfaces import IMetadataQueue
+
+generation = 6
+
+logger = __import__('logging').getLogger(__name__)
 
 
 def do_evolve(context, generation=generation):
