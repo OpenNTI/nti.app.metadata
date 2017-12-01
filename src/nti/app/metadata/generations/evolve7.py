0b254e255288eebe80d2b70cc130528e15c39f53
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-12-01 16:37:25 +0000

    set mocks ds
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.metadata/trunk@124473 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/metadata/generations/evolve7.py
+++ src/nti/app/metadata/generations/evolve7.py
@@ -15,12 +15,16 @@ import pickle
 from io import BytesIO
 
 from zope import component
+from zope import interface
 
+from zope.component.hooks import getSite
 from zope.component.hooks import setHooks
 from zope.component.hooks import site as current_site
 
 from nti.metadata import QUEUE_NAMES
 
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
 from nti.dataserver.interfaces import IRedisClient
 
 generation = 7
@@ -45,14 +49,37 @@ def _reset(redis, name, hash_key):
     return ()
 
 
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+    root_folder = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warn("Using dataserver without a proper ISiteManager.")
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
+
 def do_evolve(context, generation=generation):
     setHooks()
     conn = context.connection
     ds_folder = conn.root()['nti.dataserver']
 
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
+
     with current_site(ds_folder):
         assert component.getSiteManager() == ds_folder.getSiteManager(), \
                "Hooks not installed?"
+
+        # set root folder
+        mock_ds.root_folder = getSite().__parent__
+
         _redis = component.queryUtility(IRedisClient)
         for name in QUEUE_NAMES:
             # process jobs
@@ -64,13 +91,13 @@ def do_evolve(context, generation=generation):
                 except Exception:
                     logger.error("Cannot execute job %s", job)
             _reset(_redis, name, hash_key)
-            
-            # reset failed 
+
+            # reset failed
             name += "/failed"
             hash_key = name + '/hash'
             _reset(_redis, name, hash_key)
 
-
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
     logger.info('Metadata evolution %s done', generation)
 
 
